03d2883a79216f9f18ed7ee4cecba4359a9cd1a3
MinelaMujkic <minela_mujkic@hotmail.com>
2020-11-25 14:51:52 +0100

    layouts-master blad koji se extenda u ostale view-e koje budete pravili...potrebno dovršiti html i css za master blad prije nego se extenda u neki od drugih korisničkih view-a

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Florisa</title>
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        
+    </head>
+
+    <body>
+
+        <div class="row header">
+            Ovo je header
+        </div>
+
+
+        <div class="container">
+            @yield('content') 
+            {{--  yield ...da nam smjesti sav sadržaj koji ćemo posle definisat...zauzimamo mjesto --}}
+        </div>
+
+
+        <div class="footer">
+            Ovo je footer
+        </div>
+
+
+
+        <style>
+            .header{
+                 height: 100px;
+                background-color: blueviolet
+            }
+
+            .footer{
+            height: 100px;
+            background-color: blueviolet
+            }
+        </style>
+
+        @yield('style')
+        {{-- u view da pišemo css...zauzimamo  mjesto  --}}
+
+    </body>
+
+</html>
