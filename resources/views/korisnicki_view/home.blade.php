b6e9f737fa21b4c143bf9efffaaac9024be5cf8a
MinelaMujkic <minela_mujkic@hotmail.com>
2020-11-27 11:44:20 +0100

    privremeno sam dodala sliku sa neta, možemo je iskoristit ako hoćete meni se svidja, ne bi da nam stranica bude pretamna i sa previše boja

--- resources/views/korisnicki_view/home.blade.php
+++ resources/views/korisnicki_view/home.blade.php
@@ -1,1 +1,5 @@
 @extends('layouts.master')
+@section('content')
+<img src="https://cutewallpaper.org/21/watercolor-flowers-wallpapers/Watercolor-Flower-Wallpaper-Hd-Kayaflower.co.jpg" height="800px"/>
+    
+@endsection
