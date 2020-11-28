423730265deabb322c70e763bb4e02edd026df68
MinelaMujkic <minela_mujkic@hotmail.com>
2020-11-28 22:02:15 +0100

    funkcije za pozivanje svakog korisničkog view-a

--- app/Http/Controllers/FlorisaController.php
+++ app/Http/Controllers/FlorisaController.php
@@ -9,4 +9,20 @@ class FlorisaController extends Controller
     public function home(){
         return view("korisnicki_view.home");
         }
+
+     public function oNama(){
+         return view("korisnicki_view.o_nama");
+        }
+
+    public function cvijetniAranzmani(){
+        return view("korisnicki_view.cvijetni_aranzmani");
+         }
+    public function kucnoCvijece(){
+        return view("korisnicki_view.kucno_cvijece");
+         }
+
+    public function blog(){
+        return view("korisnicki_view.blog");
+        }    
+
 }
