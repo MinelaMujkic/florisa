f9e77457fcdcdf093b0b03d13160c28e52137e6e
AdelaM <mujicadela94@gmail.com>
2020-11-29 12:57:51 +0100

    Započela sam i shop stranu

--- resources/views/korisnicki_view/cvijetni_aranzmani.blade.php
+++ resources/views/korisnicki_view/cvijetni_aranzmani.blade.php
@@ -1,1 +1,29 @@
 @extends('layouts.master')
+@section('content')
+
+<link rel="stylesheet" href="/css/florisa.css" type="text/css">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="woocommerce columns-4 ">
+                    <div class="entry product type-product post-861 status-publish first outofstock product_cat-shop-kale has-post-thumbnail shipping-taxable purchasable product-type-simple">
+                        <a href="https://sopek.si/wp-content/uploads/2019/06/sopek-kale-1.jpg" class="woocommerce-LoopProduct-link woocommerce-loop-product__link"><img width="300" height="300" src="https://sopek.si/wp-content/uploads/2019/06/sopek-kale-1.jpg" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" srcset="https://sopek.si/wp-content/uploads/2019/06/sopek-kale-1.jpg 300w, https://sopek.si/wp-content/uploads/2019/06/sopek-kale-1.jpg 150w, https://sopek.si/wp-content/uploads/2019/06/sopek-kale-1.jpg 100w" sizes="(max-width: 300px) 100vw, 300px" /><h2 class="woocommerce-loop-product__title">Kale</h2>
+                        <span class="price"><span class="woocommerce-Price-amount amount">8.00&nbsp;<span class="woocommerce-Price-currencySymbol">KM</span></span></span>
+                    </a></div>
+                    <div class="entry product type-product post-317 status-publish instock product_cat-saksije product_cat-shop-orhideja has-post-thumbnail shipping-taxable purchasable product-type-simple">
+                        <a href="https://stil.kurir.rs/data/images/2018/04/20/15/152489_profimedia0104052911_ls.jpg" class="woocommerce-LoopProduct-link woocommerce-loop-product__link"><img width="300" height="300" src="https://stil.kurir.rs/data/images/2018/04/20/15/152489_profimedia0104052911_ls.jpg" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" srcset="https://stil.kurir.rs/data/images/2018/04/20/15/152489_profimedia0104052911_ls.jpg 300w, https://stil.kurir.rs/data/images/2018/04/20/15/152489_profimedia0104052911_ls.jpg 150w, https://stil.kurir.rs/data/images/2018/04/20/15/152489_profimedia0104052911_ls.jpg 600w, https://stil.kurir.rs/data/images/2018/04/20/15/152489_profimedia0104052911_ls.jpg 100w" sizes="(max-width: 300px) 100vw, 300px" /><h2 class="woocommerce-loop-product__title">Orhideja</h2>
+                        <span class="price"><span class="woocommerce-Price-amount amount">16.00&nbsp;<span class="woocommerce-Price-currencySymbol">KM</span></span></span>
+                    </a></div>
+                    <div class="entry product type-product post-856 status-publish instock product_cat-product_cat-shop-i-proizvodi has-post-thumbnail shipping-taxable purchasable product-type-simple">
+                        <a href="https://a1c9fd6e407b46044151-ccb4f7d4d6f45dfb3ad745c5c1495007.ssl.cf1.rackcdn.com/v4/images_flowers/thumbs/384.jpg" class="woocommerce-LoopProduct-link woocommerce-loop-product__link"><img width="300" height="300" src="https://a1c9fd6e407b46044151-ccb4f7d4d6f45dfb3ad745c5c1495007.ssl.cf1.rackcdn.com/v4/images_flowers/thumbs/384.jpg" class="attachment-woocommerce_thumbnail size-woocommerce_thumbnail" alt="" srcset="https://a1c9fd6e407b46044151-ccb4f7d4d6f45dfb3ad745c5c1495007.ssl.cf1.rackcdn.com/v4/images_flowers/thumbs/384.jpg 300w, https://a1c9fd6e407b46044151-ccb4f7d4d6f45dfb3ad745c5c1495007.ssl.cf1.rackcdn.com/v4/images_flowers/thumbs/384.jpg 150w, https://a1c9fd6e407b46044151-ccb4f7d4d6f45dfb3ad745c5c1495007.ssl.cf1.rackcdn.com/v4/images_flowers/thumbs/384.jpg 100w" sizes="(max-width: 300px) 100vw, 300px" /><h2 class="woocommerce-loop-product__title">Aranžman sa ružama</h2>
+                        <span class="price"><span class="woocommerce-Price-amount amount">25.00&nbsp;<span class="woocommerce-Price-currencySymbol">KM</span></span></span>
+                    </a></div>     
+                </div>
+            </div>
+            <div class="col-md-6">
+                Raspon cijena
+            </div>
+                
+        </div>
+    </div>
+@endsection
