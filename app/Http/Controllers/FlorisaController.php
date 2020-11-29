ad3bb6922cef276a988a3ad3d5875f082c37c2c2
MinelaMujkic <minela_mujkic@hotmail.com>
2020-11-29 01:32:10 +0100

    funkcije za unos novih ponuda u bazu, testirana, ne učitava sliku i cijenu

--- app/Http/Controllers/FlorisaController.php
+++ app/Http/Controllers/FlorisaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Ponuda;
 use Illuminate\Http\Request;
 
 class FlorisaController extends Controller
@@ -23,6 +24,43 @@ class FlorisaController extends Controller
 
     public function blog(){
         return view("korisnicki_view.blog");
-        }    
+        }   
+        
+    public function unesiPonudu(){
+        return view("admin_view.adminUnesiPonudu");
+        }  
+        
+        public function store(Request $request){
+        //    dd("$request");
+            $path = $this->uploadImage($request);
+            Ponuda::create([
+                'kategorija_cvijeca' => $request->kategorija_cvijeca,
+                'naziv' => $request->naziv,
+                "opis"=> $request->opis,
+                'slika' => $path,
+                "cijena_KM" => $request->cijena_KM,
+                "status"=> $request->status
+               
+    
+            ]);
+            return view("admin_view.adminUnesiPonudu");
+            }
+
+            private function uploadImage(Request $request) {
+                if(!$request->hasFile('slika')) {
+                    return response()->json(['upload_file_not_found'], 400);
+                }
+        
+                $file = $request->file('slika');
+                if(!$file->isValid()) {
+                    return response()->json(['invalid_file_upload'], 400);
+                }
+        
+                $path = '/uploads/'.$file->getClientOriginalName();
+        
+        
+                $file->move("uploads", $file->getClientOriginalName());
+                return $path;
+            }
 
 }
