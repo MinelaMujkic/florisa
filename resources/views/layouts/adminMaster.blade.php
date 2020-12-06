c44aa258b395e90d9a52b3fc639dd69b99353563
AdelaM <mujicadela94@gmail.com>
2020-12-06 15:07:50 +0100

    Odjava uređena da bude u liniji

--- resources/views/layouts/adminMaster.blade.php
+++ resources/views/layouts/adminMaster.blade.php
@@ -119,7 +119,7 @@
     <div class="aa-header-bottom">
       <div class="container">
         <div class="row">
-          <div class="col-md-12">
+          <div class="col-md-10">
             <div class="aa-header-bottom-area">
               <!-- logo  -->
               <div class="aa-logo">
